35c27d87171eb8381d0e928d49945e4cd3ae97a5
kosal <phoungkosal02@gmail.com>
2026-04-03 21:58:31 +0700

    Refactor MainActivity and add TopBar component

---
+++ app/src/main/java/com/t3r/studyui/screens/TopBar.kt
@@ -0,0 +1,134 @@
+package com.t3r.studyui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Screen(modifier: Modifier = Modifier) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        state = rememberTopAppBarState()
+    )
+    Scaffold(
+        modifier = modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopBar(scrollBehavior = scrollBehavior)
+        }
+    ) { paddingValues ->
+        ScreenContent(
+            paddingValues = paddingValues
+        )
+    }
+}
+
+
+@Composable
+fun ScreenContent(paddingValues: PaddingValues) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(
+            top = paddingValues.calculateTopPadding() + 16.dp,
+            bottom = paddingValues.calculateBottomPadding() + 16.dp
+        )
+    ) {
+        items(10 ) {
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+            )
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+        }
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "StudyUI",
+                color = MaterialTheme.colorScheme.onBackground.copy(0.7f),
+                fontSize = 18.sp
+
+            )
+
+        },
+        windowInsets = WindowInsets(top = 0.dp),
+        scrollBehavior = scrollBehavior,
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(100.dp)),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(0.6f),
+        ),
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Menu,
+                contentDescription = "Menu",
+                modifier = Modifier
+                    .padding(start = 14.dp, end = 8.dp)
+                    .size(27.dp)
+            )
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Filled.Notifications,
+                contentDescription = "Menu",
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(27.dp)
+            )
+            Icon(
+                imageVector = Icons.Rounded.AccountCircle,
+                contentDescription = "Profile",
+                modifier = Modifier
+                    .padding(start = 8.dp, end = 14.dp)
+                    .size(27.dp)
+            )
+        }
+    )
+}
